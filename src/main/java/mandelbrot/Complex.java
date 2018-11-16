c7f4985ed300a508d9eac23cc3f2d73ab892f5f8
s17020211 <houssem-eddine.said@etu.univ-amu.fr>
2018-11-16 15:12:22 +0100

    correction complex divide

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -171,8 +171,8 @@ public class Complex {
         }
         double m = divisor.squaredModulus();
         return new Complex(
-                (this.real * divisor.real + this.real * divisor.imaginary) / m,
-                (this.imaginary * divisor.real + this.imaginary * divisor.imaginary) / m
+                (this.real * divisor.real + this.imaginary * divisor.imaginary) / m,
+                (this.imaginary * divisor.real - this.real * divisor.imaginary) / m
         );
     }
 
